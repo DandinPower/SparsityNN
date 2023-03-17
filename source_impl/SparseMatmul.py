c7f700ef81c03687f9448a548940b389404ff417
DandinPower <tomhot246@gmail.com>
2023-03-17 15:31:03 +0800

    finish my csr matmul

--- source_impl/SparseMatmul.py
+++ source_impl/SparseMatmul.py
@@ -8,11 +8,18 @@ class Dense_Matrix:
         output = '['
         for index, row in enumerate(self.data):
             if index != 0: output += '\n '
-            output += f'{row}'
+            output += f'{row},'
+        output = output[:-1]
         output += ']'
         return output
 
-class CRS_Matrix:
+    def __mul__(self, other):
+        myData = np.array(self.data)
+        otherData = np.array(other.data)
+        answerData = np.matmul(myData, otherData)
+        return Dense_Matrix(answerData)
+
+class CSR_Matrix:
     def __init__(self, rowPtrs, colIndices, data, shape):
         self.rowPtrs = rowPtrs
         self.colIndices = colIndices
@@ -53,7 +60,7 @@ def _csr_matmult_maxnnz(nRows: int, nCols: int, Ap: list[int], Aj: list[int], Bp
         nnz += row_nnz
     return nnz
 
-def _csr_matmul(nRows: int, nCols: int, A: CRS_Matrix, B: CRS_Matrix, ans: CRS_Matrix) -> None:
+def _csr_matmul(nRows: int, nCols: int, A: CSR_Matrix, B: CSR_Matrix, ans: CSR_Matrix) -> None:
     '''
     The actual _csr_matmult function takes in the row pointers, column indices, and non-zero values 
     for both input matrices in CSR format, as well as preallocated arrays for the row pointers, column indices, 
@@ -103,31 +110,83 @@ def _csr_matmul(nRows: int, nCols: int, A: CRS_Matrix, B: CRS_Matrix, ans: CRS_M
             ans.colIndices[nnz - length:nnz], ans.data[nnz - length:nnz] = zip(*col_indices_values)
         ans.rowPtrs[i + 1] = nnz
 
-def CsrMM(A: CRS_Matrix, B: CRS_Matrix) -> CRS_Matrix:
+def _csr_matmul_my(nRows: int, nCols: int, A: CSR_Matrix, B: CSR_Matrix, ans: CSR_Matrix) -> None:
+    nnz = 0
+    ans.rowPtrs[0] = 0
+    next = np.full(nCols, -1, dtype=int)
+    sums = np.zeros(nCols, dtype=int)
+    for row in range(nRows):    
+        head = -2
+        length = 0
+        dataIndexInEachARow = range(A.rowPtrs[row], A.rowPtrs[row + 1])
+        for dataIndexInA in  dataIndexInEachARow:
+            currentDataCol = A.colIndices[dataIndexInA]
+            currentDataValue = A.data[dataIndexInA]
+            dataIndexInEachCorrespondBRow = range(B.rowPtrs[currentDataCol], B.rowPtrs[currentDataCol + 1])
+            for dataIndexInB in dataIndexInEachCorrespondBRow:
+                ansCol = B.colIndices[dataIndexInB]
+                sums[ansCol] += currentDataValue * B.data[dataIndexInB]
+                if next[ansCol] == -1:
+                    next[ansCol] = head
+                    head = ansCol
+                    length += 1
+        for _ in range(length):
+            ans.data[nnz] = sums[head]
+            ans.colIndices[nnz] = head
+            nnz += 1
+            temp = head
+            head = next[head]   
+            next[temp] = -1
+            sums[temp] = 0
+        colIndicesValuesSort = sorted(zip(ans.colIndices[nnz - length:nnz], ans.data[nnz - length:nnz]), key=lambda x: x[0])
+        if len(colIndicesValuesSort):
+            ans.colIndices[nnz - length:nnz], ans.data[nnz - length:nnz] = zip(*colIndicesValuesSort)
+        ans.rowPtrs[row + 1] = nnz
+            
+def CsrMM(A: CSR_Matrix, B: CSR_Matrix) -> CSR_Matrix:
     nRows = A.shape[0]
     nCols = B.shape[1]
     numNonZeroInC = _csr_matmult_maxnnz(nRows, nCols, A.rowPtrs, A.colIndices, B.rowPtrs, B.colIndices)
     cRowPtrs = np.zeros(nRows + 1, dtype=int)
     cColInddices = np.zeros(numNonZeroInC , dtype=int)
     cData = np.zeros(numNonZeroInC , dtype=int)
-    C = CRS_Matrix(cRowPtrs, cColInddices, cData, (nRows, nCols))
-    _csr_matmul(nRows, nCols, A, B, C)
+    C = CSR_Matrix(cRowPtrs, cColInddices, cData, (nRows, nCols))
+    _csr_matmul_my(nRows, nCols, A, B, C)
     return C
 
 def test_1():
-    A = CRS_Matrix([0, 2, 2, 3], [0, 2, 1], [1, 2, 4], (3, 3))
-    B = CRS_Matrix([0, 1, 2, 3], [1, 0, 1], [3, 1, 2], (3, 2))
+    A = CSR_Matrix([0, 2, 2, 3], [0, 2, 1], [1, 2, 4], (3, 3))
+    B = CSR_Matrix([0, 2, 2, 2], [1, 2], [3, 4], (3, 3))
+    print(A, end='\n\n')
+    print(A.to_dense(), end='\n\n')
+    print(B, end='\n\n')
+    print(B.to_dense(), end='\n\n')
     C = CsrMM(A, B)
     print(C, end='\n\n')
     print(C.to_dense(), end='\n\n')
 
 def test_2():
-    A = CRS_Matrix([0, 2, 3], [0, 2, 1], [1, 2, 3], (2, 3))
-    B = CRS_Matrix([0, 1, 2, 3], [0, 1, 0], [4, 5, 6], (3, 2))
+    A = CSR_Matrix([0, 2, 3], [0, 2, 1], [1, 2, 3], (2, 3))
+    B = CSR_Matrix([0, 1, 2, 3], [0, 1, 0], [4, 5, 6], (3, 2))
     C = CsrMM(A, B)
     print(C, end='\n\n')
     print(C.to_dense(), end='\n\n')
 
+def test_3():
+    A = CSR_Matrix([0, 2, 5, 7, 9, 10, 12, 14, 16, 16, 17],  [0, 3, 1, 4, 9, 0, 5, 2, 7, 8, 3, 6, 5, 7, 9, 1, 5],  [3, 4, 1, 2, 7, 3, 5, 8, 6, 1, 3, 9, 1, 2, 5, 2, 6], (10, 10))
+    B = CSR_Matrix([0, 2, 4, 6, 8, 10, 12, 14, 16, 16, 18], [0, 6, 2, 4, 0, 3, 1, 4, 3, 7, 0, 5, 1, 6, 2, 5, 6, 7], [4, 2, 7, 3, 5, 1, 6, 3, 4, 8, 1, 2, 5, 7, 9, 2, 6, 1], (10, 8))
+    print(A, end='\n\n')
+    print(A.to_dense(), end='\n\n')
+    print(B, end='\n\n')
+    print(B.to_dense(), end='\n\n')
+    C = CsrMM(A, B)
+    print(C, end='\n\n')
+    print(C.to_dense(), end='\n\n')
+
+    DenseC = A.to_dense() * B.to_dense()
+    print(DenseC)
+
 if __name__ == "__main__":
-    test_1()
-    test_2()
+    #test_1()
+    #test_2()
+    test_3()
